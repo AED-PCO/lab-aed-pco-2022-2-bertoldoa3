e6a7e1cad8151ddc0dfcbd203dc0bbca6a1850ce
Ana Clara Bertoldo A <101759772+bertoldoa3@users.noreply.github.com>
2022-09-29 20:22:39 -0300

    Update Lab2 e Lab3 - Código

--- codigo/Lab2.cs
+++ codigo/Lab2.cs
@@ -22,3 +22,72 @@ namespace ExercicioFatorial
         }
     }
 }
+
+Exercicio 2 -
+	using System;
+
+namespace ExercicioMultiplicacao
+{
+    internal class Program
+    {
+        static int Multiplicacao(int A, int B)
+        { 
+            int Acumulador, I=1;
+
+            if (B == 1){
+                return A;
+            }
+            return A + Multiplicacao(A, --B);
+        }
+        static void Main(string[] args)
+        {
+            int A, B, Resultado;
+            Console.WriteLine("Informe o primeiro número");
+            A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o segundo número");
+            B = int.Parse(Console.ReadLine());
+
+            Resultado = Multiplicacao(A, B);
+            Console.WriteLine("A multiplicação dos números {0} e {1} é {2}", A, B, Resultado);
+
+
+        }
+    }
+}
+
+Exercicio 3 - 
+	
+using System;
+
+namespace ExercicioPotenciacao
+{
+    internal class Program
+    {
+        static int Potenciacao(int A, int B)
+        {
+            int Acumulador, I = 1;
+
+            if (B == 1)
+            {
+                return A;
+            }
+            return A * Potenciacao(A, --B);
+        }
+        static void Main(string[] args)
+        {
+            int A, B, Resultado;
+            Console.WriteLine("Informe o primeiro número");
+            A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o segundo número");
+            B = int.Parse(Console.ReadLine());
+
+            Resultado = Potenciacao(A, B);
+            Console.WriteLine("A Potenciação dos números {0} e {1} é {2}", A, B, Resultado);
+
+
+        }
+    }
+}
+
+Exercicio 4 - 
+	
