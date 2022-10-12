3ffc6e23c825b54c439f2a62b06baee4a5b7beed
Ana Clara Bertoldo A <101759772+bertoldoa3@users.noreply.github.com>
2022-10-11 23:39:32 -0300

    Update Lab 3 - Busca Binária S/ Pós
    Acrescentando o código de busca binária sem Pós.

--- codigo/Lab3.cs
+++ codigo/Lab3.cs
@@ -1,5 +1,59 @@
 1 - Ordenação Binaria S/ Pós
 
+using System;
+
+namespace OrdBinSemPos 
+{
+    class Program
+    {
+        static void MostraVetor(int[] V)
+        {
+            for (int i = 0; i < V.Length; i++)
+            {
+                Console.WriteLine("{0}", V[i]);
+            }
+        }
+
+        static int BuscaBin(int[] V, int N, int In, int Fim)
+        {
+            int Meio = (Fim + In) / 2;
+
+            if (Fim < 0)
+            {
+                return 0;
+            }
+            if (V[Meio] < N)
+            {
+                return BuscaBin(V, N, (Meio + 1), In);
+            }
+            else if (V[Meio] > N)
+            {
+                return BuscaBin(V, N, In, (Meio - 1));
+            }                
+            else
+                return Meio;
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] V = {5, 10, 33 , 52, 90, 101, 303, 500, 503};
+            int N, Resultado;
+
+            MostraVetor(V);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Busca:");
+            N = int.Parse(Console.ReadLine());
+
+            Resultado = BuscaBin(V, N, 0, (V.Length - 1));
+
+           Console.WriteLine($"O número {N} está na posição {Resultado+1} do vetor.");
+        }
+    }
+}
+
 
 2 - Ordenação Binaria c/ Pós
 
