95f2e50b9074390748dbbc6e3f712333c9c12180
Ana Clara Bertoldo A <101759772+bertoldoa3@users.noreply.github.com>
2022-10-25 16:06:54 -0300

    Update Lab 7 - Inclusão do Exercicio 1

--- codigo/Lab7.cs
+++ codigo/Lab7.cs
@@ -1,6 +1,34 @@
 Código Exercicio1 - 
 
-####################################################### AGUARDANDO #########################################################################################
+using System;
+
+namespace Exercicio1_Laboratorio7
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+       int Igual=0, Diferente=0; 
+       string LinhaArquivo, TextoDigitado;
+
+        StreamReader sr = new StreamReader("C:\\AED\\Arq1.txt");
+        LinhaArquivo = sr.ReadLine();
+        Console.WriteLine("Escreva um texto:");
+        TextoDigitado = Console.ReadLine(); 
+
+        sr.Close();
+
+        foreach (char line in LinhaArquivo){
+          if(TextoDigitado == LinhaArquivo){
+           Igual++;
+          }
+           Diferente++;
+         }
+         Console.WriteLine ("Iguais {0}", Igual);
+         Console.WriteLine ("Diferentes {0}", Diferente);
+        }
+    }
+}
 
 Código Exercicio3 - 
 
