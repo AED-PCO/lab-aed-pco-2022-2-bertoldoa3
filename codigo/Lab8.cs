0e3ac4ce2772bc9f56f93c3bb65c01b236ce5609
Ana Clara Bertoldo A <101759772+bertoldoa3@users.noreply.github.com>
2022-11-03 19:52:28 -0300

    Update Lab 8 - Inclusão Exercício de Fila

--- codigo/Lab8.cs
+++ codigo/Lab8.cs
@@ -6,9 +6,7 @@ Exercicio 1 - Exemplo de pilha estatica
 
 Exercicio 2 - Exemplo de fila estática 
 
-using System;
-
-namespace Exercicio1_Laboratorio8_Fila
+namespace Exercicio1_ExemploFilaEstatica
 {
     internal class Program
     {
@@ -17,46 +15,52 @@ namespace Exercicio1_Laboratorio8_Fila
             Vetor[VarAux] = Valor;
             return Vetor;
         }
-        static int[] RetirarFila(int[] Vetor)
+        static int[] TirarFila(int[] Vetor)
         {
-            int VarAux = Vetor[0];
-
+            int VarAux2 = Vetor[0];
             for (int A = 1; A < Vetor.Length; A++)
+            {
                 Vetor[A - 1] = Vetor[A];
-
-            Console.WriteLine("Numero retirado" + VarAux);
+            }
+            Console.WriteLine("O elemento retirado foi: " + VarAux2);
             return Vetor;
         }
-
-
         static void Main(string[] args)
         {
-            int Pos = 0, Valor, TamanhoVetor = 5;
+            int Pos = 0, Valor = 0, TamanhoVetor = 10;
             int[] Vetor = new int[TamanhoVetor];
 
-            for (int A = 0; A < TamanhoVetor; A++)
+            for (int B = 0; B < TamanhoVetor; B++)
             {
-                Console.WriteLine("Informe o número que deseja incluir no vetor:");
+                Console.WriteLine("Informe o valor que deseja incluir no vetor");
                 Valor = int.Parse(Console.ReadLine());
-
                 if (Pos < TamanhoVetor)
                 {
-
                     InserirFila(Vetor, Pos, Valor);
-                    Pos = Pos + 1;
+                    Pos += 1;
                 }
-                Console.WriteLine("Acabaram as posições do vetor para serem preenchidas");
+                else
+                    Console.WriteLine("Vetor não tem espaço livre!");
             }
-            RetirarFila(Vetor);
-            Pos = Pos - 1;
-            Console.WriteLine("Estamos retirando na posição" + Pos);
+            TirarFila(Vetor);
+            Pos -= 1;
+            Console.WriteLine("A posição de referência é: " + Pos);
+            if (Pos > 0)
+            {
+                Console.WriteLine("Os valores na Fila são:");
+                int aux_retira = Pos;
+                for (int k = 0; k < aux_retira; k++)
+                {
+                    TirarFila(Vetor);
+                    Pos -= 1;
+                }
+            }
+            else
+                Console.WriteLine("A fila esta vazia");
 
         }
     }
 }
-
-
-
 
 
 Exercicio 3 - Exemplo de lista estática
