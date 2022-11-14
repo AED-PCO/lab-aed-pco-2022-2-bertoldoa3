f7c173cad6c831f33717889ced87192a5d1845b2
Ana Clara Bertoldo A <101759772+bertoldoa3@users.noreply.github.com>
2022-11-13 22:15:19 -0300

    Update Lab 9 - Incluindo Vetor Invertido Pilha
    Incluindo exercicio de vetor invertido de pilha.

--- codigo/Lab9.cs
+++ codigo/Lab9.cs
@@ -1,1 +1,57 @@
+Exercicio 1 - Exemplo de pilha estatica invertida
+
+
+namespace Laboratorio8_Pilha
+{
+    internal class Program
+    {
+        static int[] InserePilha(int[] Vetor, int VarAux, int Valor)
+        {
+            Vetor[VarAux] = Valor;
+            return Vetor;
+        }
+        static void InvertePilha (int[]Vetor, int TamanhoVetor)
+        {
+            int PosInverte;
+            int[] VetorInvertido = new int[TamanhoVetor];
+
+            PosInverte = TamanhoVetor;
+
+            if (PosInverte >= 0)
+            {
+                for (int I = 0; I<= TamanhoVetor; I++) {
+                    for (int A = TamanhoVetor; A > TamanhoVetor; A--)
+                    {
+                        VetorInvertido[I] = Vetor[A];
+                        Console.WriteLine("Vetor Invertido: " + VetorInvertido[I]);
+                        PosInverte--;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int Pos = 0, Valor = 0, TamanhoVetor = 5;
+            int[] Vetor = new int[TamanhoVetor];
+
+            for (int B = 0; B < TamanhoVetor; B++)
+            {
+                Console.WriteLine("Qual o valor que deseja incluir no vetor");
+                Valor = int.Parse(Console.ReadLine());
+                if (Pos < TamanhoVetor)
+                {
+
+                    InserePilha(Vetor, Pos, Valor);
+                    Pos += 1;
+                }
+                else
+                {
+                    Console.WriteLine("Vetor não tem espaço!");
+                }
+            }
+
+            InvertePilha(Vetor, TamanhoVetor);
+        }
+    }
+}
 
