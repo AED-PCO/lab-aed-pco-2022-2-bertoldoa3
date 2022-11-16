3a8eb462d84da9ecbe3159f19883b04fb3362b21
Ana Clara Bertoldo A <101759772+bertoldoa3@users.noreply.github.com>
2022-11-15 22:15:27 -0300

    Update Lab 9 - Inclusão Lista Invertida

--- codigo/Lab9.cs
+++ codigo/Lab9.cs
@@ -198,3 +198,85 @@ namespace Laboratorio8_FilaInvertida
     }
     }
 
+Exercicio 4 - Copia de metade de uma lista para outra lista (metade com a ordem original da lista e outra metade com a ordem invertida) // CODIGO COM ERRO //
+    
+using System;
+
+namespace Laboratorio8_Lista
+
+{
+    internal class Program
+    {
+        static void MostraVetor(int[] V)
+        {
+            for (int B = 0; B < V.Length; B++)
+            {
+                Console.Write($"{V[B]}, ");
+            }
+        }
+        static int[] InserirLista(int PosLimite, int[] Vetor, int Pos, int Valor)
+        {
+            if (PosLimite < 0)
+            {
+                Vetor[Pos] = Valor;
+                return Vetor;
+            }
+            if (PosLimite > Pos)
+                PosLimite = Pos;
+            for (int i = Pos; i > PosLimite; i--)
+
+            {
+                Vetor[i] = Vetor[i - 1];
+            }
+            Vetor[PosLimite] = Valor;
+            return Vetor;
+        }
+        static int[] InverteLista(int[] Vetor, int TamanhoVetor)
+        {
+            int[] VetorInvertido = new int[TamanhoVetor];
+
+            for (int J = TamanhoVetor/2; J < TamanhoVetor; J++)
+            {
+                for (int I = (TamanhoVetor - 1)/2; I >= 0; I--)
+                {
+                    VetorInvertido[I] = Vetor[J];
+                    J++;
+                }
+            }
+            return VetorInvertido;
+        }
+        static void Main(string[] args)
+        {
+            int Pos = 0, Valor = 0, TamanhoVetor = 10;
+            int[] Vetor = new int[TamanhoVetor];
+            int[] VetorInvertidoVar = new int[TamanhoVetor / 2];
+
+            for (int B = 0; B < TamanhoVetor; B++)
+            {
+                Console.WriteLine("Qual o valor que deseja incluir no vetor");
+                Valor = int.Parse(Console.ReadLine());
+                Console.WriteLine("Qual o a posição que deseja incluir no vetor");
+                Pos = int.Parse(Console.ReadLine());
+
+                if (Pos < TamanhoVetor)
+                {
+                    InserirLista(TamanhoVetor, Vetor, Pos, Valor);
+                    Pos += 1;
+                    for (int i = 0; i < Pos; i++)
+                    //Console.WriteLine("Os item da lista: " + Vetor[i] + " está em sua respectiva posição = " + i);
+                    InserirLista(TamanhoVetor, Vetor, Pos, Valor);
+                }
+                Console.Write("Lista Original: ");
+                MostraVetor(Vetor);
+
+                VetorInvertidoVar = InverteLista(Vetor, TamanhoVetor);
+
+                Console.Write("Lista Original Metade: ");
+                MostraVetor(VetorInvertidoVar);
+                Console.ReadKey();
+
+            }
+
+        }
+    }
+}
