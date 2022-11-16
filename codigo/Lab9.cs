9854232d6e092086015774f6139f557e3a1c0eb5
Ana Clara Bertoldo A <101759772+bertoldoa3@users.noreply.github.com>
2022-11-15 22:06:40 -0300

    Update Lab 8 - Inclusão FIla Invertida
    Incluindo exercicio de fila invertida.

--- codigo/Lab9.cs
+++ codigo/Lab9.cs
@@ -134,3 +134,67 @@ namespace Laboratorio8_PilhaCopiada
 }
 
 
+Exercicio 3 - Exemplo de fila estatica invertida 
+
+namespace Laboratorio8_FilaInvertida
+{
+    internal class Program
+    {
+        static void MostraVetor(int[] V)
+        {
+            for (int B = 0; B < V.Length; B++)
+            {
+                Console.Write($"{V[B]}, ");
+            }
+        }
+        static int[] InverteFila(int[] Vetor, int TamanhoVetor)
+        {
+            int[] VetorInvertido = new int[TamanhoVetor];
+
+            for (int J = 0; J < TamanhoVetor; J++)
+            {
+                for (int I = TamanhoVetor - 1; I >= 0; I--)
+                {
+                    VetorInvertido[I] = Vetor[J];
+                    J++;
+                }
+            }
+            return VetorInvertido;
+        }
+        static int[] InsereFila(int[] Vetor, int VarAux, int Valor)
+        {
+            Vetor[VarAux] = Valor;
+            return Vetor;
+        }
+        static void Main(string[] args)
+        {
+            int Pos = 0, Valor = 0, TamanhoVetor = 10;
+            int[] Vetor = new int[TamanhoVetor];
+            int[] VetorInvertidoVar = new int[TamanhoVetor];
+
+
+            for (int B = 0; B < TamanhoVetor; B++)
+            {
+                Console.WriteLine("Informe o valor que deseja incluir no vetor");
+                Valor = int.Parse(Console.ReadLine());
+                if (Pos < TamanhoVetor)
+                {
+                    InsereFila(Vetor, Pos, Valor);
+                    Pos += 1;
+                }
+                else
+                    Console.WriteLine("Vetor não tem espaço livre!");
+            }
+
+            Console.Write("Fila Sem Inversão: ");
+            MostraVetor(Vetor);
+
+            VetorInvertidoVar = InverteFila(Vetor, TamanhoVetor);
+
+            Console.Write("Fila Com Inversão: ");
+            MostraVetor(VetorInvertidoVar);
+            Console.ReadKey();
+        }
+    }
+    }
+
