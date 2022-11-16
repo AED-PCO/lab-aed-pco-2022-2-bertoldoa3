423efcf184314993d2eab5d6851628400ab0b036
Ana Clara Bertoldo A <101759772+bertoldoa3@users.noreply.github.com>
2022-11-15 22:00:38 -0300

    Update Lab - Incluindo Pilha
    Inclusão dos exercicios de pilha invertida e copia de pilha.

--- codigo/Lab9.cs
+++ codigo/Lab9.cs
@@ -1,30 +1,109 @@
-Exercicio 1 - Exemplo de pilha estatica invertida ############################## CODIGO COM ERRO - AGUARDANDO CORRECAO (SO MOSTRA A ULTIMA POSIÇÃO DA PILHA INVERTIDA)
+Exercicio 1 - Exemplo de pilha estatica invertida 
 
 namespace Laboratorio8_PilhaInvertida
 {
     internal class Program
     {
+        static void MostraVetor(int[] V)
+        {
+            for (int B = 0; B < V.Length; B++)
+            {
+                Console.Write($"{V[B]}, ");
+            }
+        }
+
         static int[] InserePilha(int[] Vetor, int VarAux, int Valor)
         {
             Vetor[VarAux] = Valor;
             return Vetor;
         }
-        static void InvertePilha(int[] Vetor, int TamanhoVetor)
+        static int[] InvertePilha(int[] Vetor, int TamanhoVetor)
         {
             int[] VetorInvertido = new int[TamanhoVetor];
-            for (int A = TamanhoVetor - 1; A >= 0; A--)
+
+            for (int J = 0; J < TamanhoVetor; J++)
+            {
+                for (int I = TamanhoVetor - 1; I >= 0; I--)
+                {
+                    VetorInvertido[I] = Vetor[J];
+                    J++;
+                }
+            }
+            return VetorInvertido;
+        }
+        static void Main(string[] args)
+            {
+                int Pos = 0, Valor = 0, TamanhoVetor = 5;
+                int[] Vetor = new int[TamanhoVetor];
+                int[] VetorInvertidoVar = new int[TamanhoVetor];
+
+
+                for (int B = 0; B < TamanhoVetor; B++)
+                {
+                    Console.WriteLine("Qual o valor que deseja incluir no vetor");
+                    Valor = int.Parse(Console.ReadLine());
+
+                    if (Pos < TamanhoVetor)
+                    {
+                        InserePilha(Vetor, Pos, Valor);
+                        Pos += 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Vetor sem espaço, remova itens para incluir.");
+                    }
+                }
+
+            Console.Write("Pilha Sem Inversão: ");
+            MostraVetor(Vetor);
+
+            VetorInvertidoVar = InvertePilha(Vetor, TamanhoVetor);
+
+            Console.Write("Pilha Com Inversão: ");
+            MostraVetor(VetorInvertidoVar);
+            Console.ReadKey();
+        }
+        }
+    }
+
+Exercicio 2 - Exemplo de cópia de pilha estatica original
+
+namespace Laboratorio8_PilhaInvertida
+{
+    internal class Program
+    {
+        static void MostraVetor(int[] V)
+        {
+            for (int B = 0; B < V.Length; B++)
+            {
+                Console.Write($"{V[B]}, ");
+            }
+        }
+        static int[] InserePilha(int[] Vetor, int VarAux, int Valor)
+        {
+            Vetor[VarAux] = Valor;
+            return Vetor;
+        }
+        static int[] CopiaPilha(int[] Vetor, int TamanhoVetor)
+        {
+            int[] VetorInvertido = new int[TamanhoVetor];
+
+            for (int J = 0; J < TamanhoVetor; J++)
             {
                 for (int I = 0; I < TamanhoVetor; I++)
                 {
-                  VetorInvertido[A] = Vetor[I];
-                 }
-                Console.WriteLine(VetorInvertido[A]);
+                    VetorInvertido[I] = Vetor[J];
+                    J++;
+                }
             }
+            return VetorInvertido;
         }
         static void Main(string[] args)
         {
             int Pos = 0, Valor = 0, TamanhoVetor = 5;
             int[] Vetor = new int[TamanhoVetor];
+            int[] VetorCopiadoVar = new int[TamanhoVetor];
+
 
             for (int B = 0; B < TamanhoVetor; B++)
             {
@@ -41,7 +120,17 @@ namespace Laboratorio8_PilhaInvertida
                     Console.WriteLine("Vetor sem espaço, remova itens para incluir.");
                 }
             }
-            InvertePilha(Vetor, TamanhoVetor);
+
+            Console.Write("Pilha Original: ");
+            MostraVetor(Vetor);
+
+            VetorCopiadoVar = CopiaPilha(Vetor, TamanhoVetor);
+
+            Console.Write("Pilha Copiada: ");
+            MostraVetor(VetorCopiadoVar);
+            Console.ReadKey();
         }
     }
 }
+
+
