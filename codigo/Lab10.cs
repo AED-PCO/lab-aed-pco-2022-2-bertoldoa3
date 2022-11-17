08bd4efe21948a075f056effdcbbab725802e268
Ana Clara Bertoldo A <101759772+bertoldoa3@users.noreply.github.com>
2022-11-17 17:29:26 -0300

    Update Lab 10 - Fila
    Ajustando codigo de fila com classes.

--- codigo/Lab10.cs
+++ codigo/Lab10.cs
@@ -1,112 +1,124 @@
 Exercicio 1 - Fila Utilizando Classes
 
-using System;//CLASSE FILA
+//////////////////////////////////////////////////////////////////////////////// MAIN ////////////////////////////////////////////////////////////////////////////////
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjetoClasse
-{
-    internal class Fila
-    {
-        const int Capacidade = 5;
-        private int quantidade = 0;
-        private int inicio = 0;
-        private int fim = 0;
-        private string[] dados = new string[Capacidade];
-
-        public int tamanho()
-        {
-            return quantidade;
-        }
-
-        public void inserirElemento(string valor_i)
-        {
-            if(tamanho () == Capacidade)
-            {
-                throw new Exception("Fila cheia");
-            }
-            else
-            {
-                dados[fim] = valor_i;  
-                fim = (fim+1) % Capacidade;
-                quantidade++;
-            }
-        }
-        public string RemoveElemento()
-        {
-            if (tamanho() == 0)
-            {
-                throw new Exception("Fila Vazia");
-            }
-            else
-            {
-                String valor = dados[inicio];
-                inicio = (inicio +1) % Capacidade;
-                quantidade--;
-                return valor;
-            }
-        }
-    }
-}
-
-using System;//MAIN FILA
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProjetoClasse
+namespace ClasseFila
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string op = "", valor;
+            string Opcao, Valor;
 
-            Fila  minhafila = new Fila();
-            Console.WriteLine("Sistemas de Fila");
+            Fila Fila_Aed = new Fila();
 
             do
             {
-                try
-                {
-                    Console.WriteLine("\n1 -> inserir \n2 -> remover \n3 -> mostrar");
-                    op = Console.ReadLine();
+                Console.WriteLine("");
+                Console.WriteLine("Bem vindo ao programa de filas.");
+                Console.WriteLine("Lembre-se o primeiro a chegar é o primeiro a ir embora! Chegue cedo e evite filas");
+                Console.WriteLine("Menu para utilizar o programa:");
+                Console.WriteLine("1 - Inserir");
+                Console.WriteLine("2 - Remover");
+                Console.WriteLine("3 - Quantidade de Pessoas na Fila");
+                Console.WriteLine("");
 
-                    switch (op)
+                Opcao = Console.ReadLine();
+
+                    switch (Opcao)
                     {
                         case "1":
                             Console.Write("Qual o elemento para inserir na fila: ");
-                            valor = Console.ReadLine();
-                            minhafila.inserirElemento(valor);
+                            Valor = Console.ReadLine();
+                            Fila_Aed.InserirElemento(Valor);
                             break;
 
                         case "2":
-                            Console.Write("Qual o elemento para Remover na fila: ");
-                            valor = Console.ReadLine();
-                            minhafila.RemoveElemento();
+                            Console.Write("Removendo o primeiro elemento na fila");
+                            Fila_Aed.RemoveElemento();
                             break;
 
                         case "3":
                             Console.Write("Tamanho da fila: ");
-                            minhafila.tamanho();
+                            Fila_Aed.Tamanho();
                             break;
 
                         default:
                             Console.WriteLine("Opção inválida!!");
                             break;
                     }
-                }
-                catch (Exception erro)
-                {
-                    Console.WriteLine(erro.Message);
-                }
-            } while (op != "9");
+             } while (Opcao != "9");
         }
     }
 }
+
+//////////////////////////////////////////////////////////////////////////////// CLASSE FILA ////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseFila_Dinamica
+{
+    internal class Fila
+    {
+        const int Capacidade = 5;
+        private int Quant = 0;
+        private int Inicio = 0;
+        private int Fim = 0;
+        private string[] DadosFila = new string[Capacidade];
+
+        public int Tamanho()
+        {   
+            Console.WriteLine(Quant);
+            return Quant;
+        }
+
+        public void InserirElemento(string Valor)
+        {
+            if (Tamanho() == Capacidade)
+            {
+                Console.WriteLine("Fila cheia");
+            }
+            else
+            {
+                DadosFila[Fim] = Valor; // O vetor recebe o valor digitado na ultima posição.
+                Fim = (Fim + 1) % Capacidade; // Depois de inserir na ultima posição, inclui na variavel fim o seu valor acrescido em 1
+                                              // e divide pelo resto.
+                Quant++; // Quantidade recebe mais um pois é um acumulador que mostra em outra função a quantidade
+                         // De numeros na fila
+            }
+        }
+        public string RemoveElemento()
+        {
+            string Valor;
+            if (Tamanho() == 0)
+            {
+                return "Fila vazia";
+                
+            }
+            else
+            {
+                Valor = DadosFila[Fim]; // A variavel valor, recebe 0, o valor pré definido para a variavel FIM.
+                Inicio = (Inicio + 1) % Capacidade; // Depois de remover na primeira posição baseado na lei da fila,
+                                                    // inclui na variavel inicio o seu valor acrescido em 1
+                                                    // e divide pelo resto da capacidade.
+                Quant--; // Quantidade recebe ela menos um para mostrar que agora tem menos um número na fila.
+                return Valor;
+            }
+        }
+    }
+}
+
 
 Exercicio 2 - Pilha utilizando classes
 
